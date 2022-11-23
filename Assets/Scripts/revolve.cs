1fae65b7d28fe7b24171ae27912b07ff010445fd
YUHAO WANG <wyh19990829@gmail.com>
2022-11-23 16:02:01 +0100

    Update revolve.cs

--- Assets/Scripts/revolve.cs
+++ Assets/Scripts/revolve.cs
@@ -6,8 +6,8 @@ public class revolve : MonoBehaviour
 {
     public float speed;
     public GameObject center;
-    private int offset_x;//法平面的偏移角x
-    private int offset_y;//法平面的偏移角y
+    private int offset_x;
+    private int offset_y;
     // Start is called before the first frame update
     void Start()
     {
